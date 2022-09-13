6390210cb4696086379078b91b82086b50be6605
Rhys Malyon <malyon@housingjapan.com>
2022-09-13 18:12:54 +0900

    Add users route

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 
 import PlacesRoutes from './modules/places/routes'
+import UsersRoutes from './modules/users/routes'
 
 const router = Router()
 
 router.use('/places', PlacesRoutes)
+router.use('/users', UsersRoutes)
 
 export default router

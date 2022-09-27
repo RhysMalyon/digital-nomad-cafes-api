05a60c7d4a1aa8f6fde448fb5f48d0e58e14eeaf
Rhys Malyon <malyon@housingjapan.com>
2022-09-27 12:44:09 +0900

    Update auth middleware for JWT handling

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,47 +1,99 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @types/jsonwebtoken incorrectly handles the key property of jwt functions
+// KeyObject is a valid input but incorrectly raises a type error
+
+import * as crypto from 'crypto'
+import { createPrivateKey, createPublicKey } from 'crypto'
 import { NextFunction, Request, Response } from 'express'
+import * as fs from 'fs'
+import * as jwt from 'jsonwebtoken'
+import * as path from 'path'
+import { User } from '../modules/users/types'
 import { validateToken } from '../utils/jwt'
+
+const privateKey = createPrivateKey({
+    key: fs.readFileSync(path.join(__dirname, './../../private.key')),
+    passphrase: process.env.PRIVATE_KEY_PASSPHRASE,
+})
+
+const publicKey = createPublicKey(privateKey)
 
 /**
  * middleware to check whether user has access to a specific endpoint
  *
- * @param allowedAccessTypes list of allowed access types of a specific endpoint
+ * @param allowedRoles list of allowed access types of a specific endpoint
  */
 export const authorize =
-    (allowedAccessTypes: string[]) =>
+    (allowedRoles: string[]) =>
     async (req: Request, res: Response, next: NextFunction) => {
         try {
             let jwt = req.headers.authorization
 
-            // verify request has token
+            // Verify request has token
             if (!jwt) {
-                return res.status(401).json({ message: 'Invalid token' })
+                return res.status(401).json({ message: 'Invalid token.' })
             }
 
-            // remove Bearer if using Bearer Authorization mechanism
+            // Remove Bearer if using Bearer Authorization mechanism
             if (jwt.toLowerCase().startsWith('bearer')) {
                 jwt = jwt.slice('bearer'.length).trim()
             }
 
-            // verify token hasn't expired yet
+            // Verify token hasn't expired
             const decodedToken = await validateToken(jwt)
 
-            const hasAccessToEndpoint = allowedAccessTypes.some((at) =>
-                decodedToken.accessTypes.some((uat) => uat === at)
+            const hasAccessToEndpoint = allowedRoles.some((role) =>
+                decodedToken.role === role
             )
 
             if (!hasAccessToEndpoint) {
                 return res.status(401).json({
-                    message: 'Not enough privileges to access endpoint',
+                    message: 'Insufficient privileges.',
                 })
             }
 
             next()
         } catch (error) {
             if (error.name === 'TokenExpiredError') {
-                res.status(401).json({ message: 'Expired token' })
+                res.status(401).json({ message: 'Expired token.' })
                 return
             }
 
-            res.status(500).json({ message: 'Failed to authenticate user' })
+            res.status(500).json({ message: 'Failed to authenticate user.' })
         }
     }
+
+export const generateAccessToken = (user: User['username'], role: string) => {
+    const signOptions: jwt.SignOptions = {
+        algorithm: 'RS256',
+        expiresIn: '15m',
+    }
+
+    // @ts-ignore
+    return jwt.sign({ user: user, role: role }, privateKey, signOptions)
+}
+
+export const generateRefreshToken = (user: User['username'], role: string) => {
+    const tokenId = crypto.randomBytes(40).toString('hex')
+
+    const signOptions: jwt.SignOptions = {
+        algorithm: 'RS256',
+        expiresIn: '20m',
+        jwtid: tokenId,
+    }
+
+    // @ts-ignore
+    return jwt.sign({ user: user, role: role }, privateKey, signOptions)
+}
+
+export const decodeRefreshToken = (refreshToken: string) => {
+    try {
+        // @ts-ignore
+        const decoded = jwt.verify(refreshToken, publicKey, {
+            algorithms: ['RS256'],
+        })
+        return decoded
+    } catch (err) {
+        console.log(err)
+    }
+}
